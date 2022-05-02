4b40c2928e8051e66b349c93d78284e8768166ee
Pavan Kumar <pkumar.rathor@supportgov.in>
2022-05-02 14:21:01 +0530

    app code update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 require("./config/database").connect();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const express = require("express");
 
 const app = express();
